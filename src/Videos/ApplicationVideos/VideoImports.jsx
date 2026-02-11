2d00dbb2abb71452abd2688aa5f8abd6c24d6f6e
Target <fastmovingtarget220689@gmail.com>
2026-02-11 14:32:44 +0000

    Turns out this should exist

--- src/Videos/ApplicationVideos/VideoImports.jsx
+++ src/Videos/ApplicationVideos/VideoImports.jsx
@@ -1,180 +1,109 @@
-import appOpen from "./01-Open.mp4"
-import appCirclePush from "./02-CircleAndPush.mp4"
-import appSimpleUpwardTurn from "./03-SimpleUpwardTurn.mp4"
-import appLCW from "./04-Lan'chur'wei.mp4"
-import appCircleBackAndPush from "./05-CircleBackAndPush.mp4"
-import appMonkey from "./06-Monkey.mp4"
-import appHorse from "./07-Horse.mp4"
-import appBear from "./08-Bear.mp4"
-import appFlickWristUp from "./09-FlickWristUp.mp4"
-import appCircleArmsDownThenUp from "./10-CircleArmsDownThenUp.mp4"
-import appTurnWithArmsVertical from "./11-TurnWithArmsVertical.mp4"
-import appFlurbleReady from "./12-FlurbleReady.mp4"
-import appFlurblePush from "./13-FlurblePush.mp4"
-import appPlayHarp from "./14-PlayHarp.mp4"
-import appCircleDownWithHarp from "./15-CircleDownWithHarp.mp4"
-import appPunchHand from "./16-PunchHand.mp4"
-import appStirCauldron from "./17-StirCauldron.mp4"
-import appFacepalmOfDOOM from "./18-FacepalmOfDOOM.mp4"
-import appDeflectAndPunch from "./19-DeflectAndPunch.mp4"
-import appRightLeftRightMiddle from "./20-RightLeftRightMiddle.mp4"
-import appDownUpDown from "./21-DownUpDown.mp4"
-import appKnuckleToKnee from "./22-KnuckleToKnee.mp4"
-import app90Degrees from "./23-90Degrees.mp4"
-import appHoldTiger from "./24-HoldTiger.mp4"
-import appTurnArm from "./25-TurnArm.mp4"
-import appSwingTurnIn from "./26-SwingTurnIn.mp4"
-import appPushAndStepBack from "./27-PushAndStepBack.mp4"
-import appYinAndYang from "./28-YinAndYang.mp4"
-import appLowerBody from "./29-LowerBody.mp4"
-import appTakeOffJacket from "./30-TakeOffJacket.mp4"
-import appDrawBow from "./31-DrawBow.mp4"
-import appStepBack from "./32-StepBack.mp4"
-import appFistPump from "./33-FistPump.mp4"
-import appBasketball from "./34-Basketball.mp4"
-import appCloudHands from "./35-CloudHands.mp4"
-
 const vidList = [{
     title:"Open",
     text:"Description of Taiji Open Applications",
-    video:appOpen
 }, {
     title:"Grasp Half Moon", 
     text:"Description",
-    video:appCirclePush
 }, {
     title:"Turn and Seek Grail", 
     text:"Description",
-    video:appSimpleUpwardTurn
 }, {
     title:"Lan Que Wei", 
     text:"Description",
-    video:appLCW
 }, {
     title:"Circle Back and Push", 
     text:"Description",
-    video:appCircleBackAndPush
 }, {
     title:"Hold Whip", 
     text:"Description",
-    video:appHorse
 },{
     title:"Monkey on the Tree", 
     text:"Description", 
-    video:appMonkey
 }, {
     title:"Hold Spear", 
     text:"Description", 
-    video:appBear
 }, {
     title:"Spin Lasoo", 
     text:"Description", 
-    video:appFlickWristUp
 }, {
     title:"Close Feet, Turn Wrist", 
     text:"Description", 
-    video:appCircleArmsDownThenUp
 }, {
     title:"Close hands and feet", 
     text:"Description", 
-    video:appTurnWithArmsVertical
 }, {
     title:"Brush Coat from leg: 1", 
     text:"Description", 
-    video:appFlurbleReady
 }, {
     title:"Brush Coat from leg: 2", 
     text:"Description", 
-    video:appFlurblePush
 }, {
     title:"Play Harp", 
     text:"Description", 
-    video:appPlayHarp
 }, {
     title:"Pick up stone and plug hole in the sky", 
     text:"Description", 
-    video:appCircleDownWithHarp
 }, {
     title:"Walk in, stir cauldron: Part 1", 
     text:"Description", 
-    video:appPunchHand
 }, {
     title:"Walk in, stir cauldron: Part 2", 
     text:"Description", 
-    video:appStirCauldron
 }, {
     title:"Sit down on horse, hold a sword", 
     text:"Description", 
-    video:appFacepalmOfDOOM
 }, {
     title:"Stab with Sword", 
     text:"Description", 
-    video:appDeflectAndPunch
 }, {
     title:"Stupid Man Moves Mountain", 
     text:"Description", 
-    video:appRightLeftRightMiddle,
 }, {
     title:"Golden Dragon Turns its Tail", 
     text:"Description", 
-    video:appDownUpDown
 }, {
     title:"Sit on Stone", 
     text:"Description", 
-    video:appKnuckleToKnee
 }, {
     title:"Cross Hands", 
     text:"Description", 
-    video:app90Degrees
 }, {
     title:"Hold tiger's head, throw back into forest", 
     text:"Description", 
-    video:appHoldTiger
 }, {
     title:"Turn Arm", 
     text:"Description", 
-    video:appTurnArm
 }, {
     title:"Jump onto Horse", 
     text:"Description", 
-    video:appSwingTurnIn
 }, {
     title:"Push and Step back", 
     text:"Description", 
-    video:appPushAndStepBack
 }, {
     title:"Paint Yin and Yang", 
     text:"Description", 
-    video:appYinAndYang
 }, {
     title:"Go back, trap monkey", 
     text:"Description", 
-    video:appLowerBody
 }, {
     title:"Find needle on the sea floor", 
     text:"Description", 
-    video:appTakeOffJacket
 }, {
     title:"Draw Bow: 1", 
     text:"Description", 
-    video:appDrawBow
 }, {
     title:"Draw Bow: 2", 
     text:"Description", 
-    video:appStepBack,
 }, {
     title:"Throw Hammer", 
     text:"Description", 
-    video:appFistPump
 }, {
     title:"Throw Basketball", 
     text:"Description", 
-    video:appBasketball
 
 }, {
     title:"Cloud Hands", 
     text:"Description", 
-    video:appCloudHands
 }]
 
 export default vidList; 
